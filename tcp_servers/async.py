a283406553b3f37cca7e82f64723c9684b43b95e
Kazakov <abatari@mail.ru>
2015-09-30 21:11:55 +0300

    Update async.py

--- tcp_servers/async.py
+++ tcp_servers/async.py
@@ -1,5 +1,5 @@
 #!/usr/local/bin/python
-
+#это неблокрирующие сокеты: в этом случае, если процессу нечем заняться, то он не блокируется, а продолжает быть доступным
 import socket
 import select
 
@@ -16,6 +16,7 @@ excepts = []
 
 while 1:
     input_ready, output_ready, except_ready = select.select(list(inputs), outputs.keys(), excepts, 0.5)
+    #формируем три списка сокетов (сокеты, в которые нам пишут; откуда нас читают; 
     for s in input_ready:
         if s == server_socket:
             client_socket, remote_address = server_socket.accept()
@@ -24,7 +25,8 @@ while 1:
         else:
             request = s.recv(1024)
             print '{} : {}'.format(s.getpeername(), request)
-            outputs[s] = request.upper()
+            outputs[s] = request.upper()#вот он минус данного подхода: если в этом месте вместо простейшей операции будет рендеринг видео,
+            #то завершения операции будут ждать все. т.о. нельзя использовать надолго блокирующие операции при неблокирующем подходе
             inputs.remove(s)
     for s in output_ready:
         if s in outputs:
